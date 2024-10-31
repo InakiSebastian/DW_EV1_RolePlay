8179b07c3b41ba2719bb42c828e4a2f61682e7db
InakiSebastian <inakicabrerao@gmail.com>
2024-10-31 12:22:56 +0100

    CORRECTION: CONTENIDO DE TEMPLATE HEADER.PHP ERRONEAMENTE EN INDEX.PHP.

---
+++ app/views/templates/header.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>RolePlay</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+
+    <nav class="navbar border-bottom navbar-expand-lg  bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand border-end p-3" href="index.php">ROLEPLAY</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">CREAR CRIATURA</a>
+                    </li>
+                   
+                </ul>
+            </div>
+        </div>
+    </nav>
