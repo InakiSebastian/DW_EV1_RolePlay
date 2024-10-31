0e3769eeb2d22a359083325b291980699ba366fc
InakiSebastian <inakicabrerao@gmail.com>
2024-10-31 12:30:55 +0100

    FEATURE: AÑADIENDO FUNCIONALIDAD BASICA A INDEX.PHP PUBLICO

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -1,33 +1,13 @@
 <?php
-
+require_once dirname(__FILE__) . '\..\templates\header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <title>RolePlay</title>
-        <!-- Bootstrap Core CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    </head>
-
-    <nav class="navbar border-bottom navbar-expand-lg  bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand border-end p-3" href="index.php">ROLEPLAY</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">CREAR CRIATURA</a>
-                    </li>
-                   
-                </ul>
-            </div>
-        </div>
-    </nav>
+<body>
+    
+    <h1 class="text-center">Comunidad de RolePlay</h1>
+    <hr>
+    <h2 class="text-center">Abajo se encuentran las criaturas</h2>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
